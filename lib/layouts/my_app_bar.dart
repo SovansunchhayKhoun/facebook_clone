197ba9a666c00c504445ce32c8c24734ab710d3b
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-06-29 17:14:37 +0900

    Project Setup

---
+++ lib/layouts/my_app_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('AppBar');
+  }
+}
---
+++ lib/layouts/my_bottom_navigation_bar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyBottomNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello');
+  }
+}
